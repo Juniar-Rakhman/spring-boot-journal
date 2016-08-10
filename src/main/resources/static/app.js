947aa8b293f6bee95ad5c086ebbf7e84a4072723
juniar_rakhman <juniar.rahman@gmail.com>
2016-08-10 15:36:34 +0700

    Successfully Implement ReactJS

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,70 +1,284 @@
 'use strict';
 
-// tag::vars[]
 const React = require('react');
 const client = require('./client');
-// end::vars[]
 
-// tag::app[]
+const follow = require('./follow'); // function to hop multiple links by "rel"
+
+const root = '/api';
+
 class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {journalEntries: []};
+        this.state = {journalEntries: [], attributes: [], pageSize: 2, links: {}};
+        this.updatePageSize = this.updatePageSize.bind(this);
+        this.onCreate = this.onCreate.bind(this);
+        this.onDelete = this.onDelete.bind(this);
+        this.onNavigate = this.onNavigate.bind(this);
     }
 
-    componentDidMount() {
-        client({method: 'GET', path: '/api/journalEntries'}).done(response => {
-            this.setState({journalEntries: response.entity._embedded.journalEntries});
+    // tag::follow-2[]
+    loadFromServer(pageSize) {
+        follow(client, root, [
+            {rel: 'journalEntries', params: {size: pageSize}}]
+        ).then(journalCollection => {
+            return client({
+                method: 'GET',
+                path: journalCollection.entity._links.profile.href,
+                headers: {'Accept': 'application/schema+json'}
+            }).then(schema => {
+                this.schema = schema.entity;
+                return journalCollection;
+            });
+        }).done(journalCollection => {
+            this.setState({
+                journalEntries: journalCollection.entity._embedded.journalEntries,
+                attributes: Object.keys(this.schema.properties),
+                pageSize: pageSize,
+                links: journalCollection.entity._links});
         });
     }
+    // end::follow-2[]
+
+    // tag::create[]
+    onCreate(newEntry) {
+        follow(client, root, ['journalEntries']).then(journalCollection => {
+            return client({
+                method: 'POST',
+                path: journalCollection.entity._links.self.href,
+                entity: newEntry,
+                headers: {'Content-Type': 'application/json'}
+            })
+        }).then(response => {
+            return follow(client, root, [
+                {rel: 'journalEntries', params: {'size': this.state.pageSize}}]);
+        }).done(response => {
+            this.onNavigate(response.entity._links.last.href);
+        });
+    }
+    // end::create[]
+
+    // tag::delete[]
+    onDelete(journalEntry) {
+        client({method: 'DELETE', path: journalEntry._links.self.href}).done(response => {
+            this.loadFromServer(this.state.pageSize);
+        });
+    }
+    // end::delete[]
+
+    // tag::navigate[]
+    onNavigate(navUri) {
+        client({method: 'GET', path: navUri}).done(journalCollection => {
+            this.setState({
+                journalEntries: journalCollection.entity._embedded.journalEntries,
+                attributes: this.state.attributes,
+                pageSize: this.state.pageSize,
+                links: journalCollection.entity._links
+            });
+        });
+    }
+    // end::navigate[]
+
+    // tag::update-page-size[]
+    updatePageSize(pageSize) {
+        if (pageSize !== this.state.pageSize) {
+            this.loadFromServer(pageSize);
+        }
+    }
+    // end::update-page-size[]
+
+    // tag::follow-1[]
+    componentDidMount() {
+        this.loadFromServer(this.state.pageSize);
+    }
+    // end::follow-1[]
 
     render() {
         return (
-            <JournalEntries journalEntries={this.state.journalEntries}/>
+            <div>
+                <CreateDialog attributes={this.state.attributes} onCreate={this.onCreate}/>
+                <JournalEntries journalEntries={this.state.journalEntries}
+                              links={this.state.links}
+                              pageSize={this.state.pageSize}
+                              onNavigate={this.onNavigate}
+                              onDelete={this.onDelete}
+                              updatePageSize={this.updatePageSize}/>
+            </div>
         )
     }
 }
-// end::app[]
 
-// tag::journal-list[]
-class JournalEntries extends React.Component {
+// tag::create-dialog[]
+class CreateDialog extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        var newEntry = {};
+        this.props.attributes.forEach(attribute => {
+            newEntry[attribute] = React.findDOMNode(this.refs[attribute]).value.trim();
+        });
+        this.props.onCreate(newEntry);
+
+        // clear out the dialog's inputs
+        this.props.attributes.forEach(attribute => {
+            React.findDOMNode(this.refs[attribute]).value = '';
+        });
+
+        // Navigate away from the dialog to hide it.
+        window.location = "#";
+    }
+
     render() {
-        var journalEntries = this.props.journalEntries.map(journal =>
-            <Journal key={journal._links.self.href} journal={journal}/>
+        var inputs = this.props.attributes.map(attribute =>
+            <p key={attribute}>
+                <input type="text" placeholder={attribute} ref={attribute} className="field" />
+            </p>
         );
+
         return (
-            <table>
-                <tr>
-                    <th>Title</th>
-                    <th>Created Date</th>
-                    <th>Summary</th>
-                </tr>
-                {journalEntries}
-            </table>
+            <div>
+                <a href="#createJournalEntry">Create</a>
+
+                <div id="createJournalEntry" className="modalDialog">
+                    <div>
+                        <a href="#" title="Close" className="close">X</a>
+
+                        <h2>Create new journal entry</h2>
+
+                        <form>
+                            {inputs}
+                            <button onClick={this.handleSubmit}>Create</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         )
     }
-}
-// end::journal-list[]
 
-// tag::journal[]
-class Journal extends React.Component {
+}
+// end::create-dialog[]
+
+class JournalEntries extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleNavFirst = this.handleNavFirst.bind(this);
+        this.handleNavPrev = this.handleNavPrev.bind(this);
+        this.handleNavNext = this.handleNavNext.bind(this);
+        this.handleNavLast = this.handleNavLast.bind(this);
+        this.handleInput = this.handleInput.bind(this);
+    }
+
+    // tag::handle-page-size-updates[]
+    handleInput(e) {
+        e.preventDefault();
+        var pageSize = React.findDOMNode(this.refs.pageSize).value;
+        if (/^[0-9]+$/.test(pageSize)) {
+            this.props.updatePageSize(pageSize);
+        } else {
+            React.findDOMNode(this.refs.pageSize).value =
+                pageSize.substring(0, pageSize.length - 1);
+        }
+    }
+    // end::handle-page-size-updates[]
+
+    // tag::handle-nav[]
+    handleNavFirst(e){
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.first.href);
+    }
+
+    handleNavPrev(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.prev.href);
+    }
+
+    handleNavNext(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.next.href);
+    }
+
+    handleNavLast(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.last.href);
+    }
+    // end::handle-nav[]
+
+    // tag::journalEntry-list-render[]
+    render() {
+        var journalEntries = this.props.journalEntries.map(journalEntry =>
+            <JournalEntry key={journalEntry._links.self.href} journalEntry={journalEntry} onDelete={this.props.onDelete}/>
+        );
+
+        var navLinks = [];
+        if ("first" in this.props.links) {
+            navLinks.push(<button key="first" onClick={this.handleNavFirst}>&lt;&lt;</button>);
+        }
+        if ("prev" in this.props.links) {
+            navLinks.push(<button key="prev" onClick={this.handleNavPrev}>&lt;</button>);
+        }
+        if ("next" in this.props.links) {
+            navLinks.push(<button key="next" onClick={this.handleNavNext}>&gt;</button>);
+        }
+        if ("last" in this.props.links) {
+            navLinks.push(<button key="last" onClick={this.handleNavLast}>&gt;&gt;</button>);
+        }
+
+        return (
+            <div>
+                <input ref="pageSize" defaultValue={this.props.pageSize} onInput={this.handleInput}/>
+                <table>
+                    <tr>
+                        <th>Title</th>
+                        <th>Creation Date</th>
+                        <th>Summary</th>
+                        <th></th>
+                    </tr>
+                    {journalEntries}
+                </table>
+                <div>
+                    {navLinks}
+                </div>
+            </div>
+        )
+    }
+    // end::journalEntry-list-render[]
+}
+
+// tag::journalEntry[]
+class JournalEntry extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    handleDelete() {
+        this.props.onDelete(this.props.journalEntry);
+    }
+
     render() {
         return (
             <tr>
-                <td>{this.props.journal.title}</td>
-                <td>{this.props.journal.createdAsShort}</td>
-                <td>{this.props.journal.summary}</td>
+                <td>{this.props.journalEntry.title}</td>
+                <td>{this.props.journalEntry.createdAsShort}</td>
+                <td>{this.props.journalEntry.summary}</td>
+                <td>
+                    <button onClick={this.handleDelete}>Delete</button>
+                </td>
             </tr>
         )
     }
 }
-// end::journal[]
+// end::journalEntry[]
 
-// tag::render[]
 React.render(
     <App />,
     document.getElementById('react')
-);
-// end::render[]
-
+)
